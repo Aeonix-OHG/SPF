3fca7c1e7351d042fe96050e62961cc524b8e37f
Bonbon <mail@jan-schmitt.eu>
2023-12-28 11:36:13 +0100

    fist working version

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Neat Picture Format
 ## Concept
 The concept of NPF is to create a simple file in which are multiple multiple colors values for individiual pixels wich are seperated by a s character for the next value, a p character for next pixel or a n charcter for next line. For Example
 ```Concept
-255s051s047n000s000s000n
-000s000s000n036s137s255
+255s051s047p000s000s000n
+000s000s000p036s137s255
 ```
+## Programm
+This programm is a simple concept for making a png to a .npf. It uses Image dependeci.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use image::{GenericImageView, Pixel};
+use std::fs::File;
+use std::io::Write;
+
+fn main() -> std::io::Result<()> {
+ let mut file = File::create("testpic.npf")?;
+ let img = image::open("testpic.png").map_err(|e| {
+     std::io::Error::new(std::io::ErrorKind::Other, e.to_string())
+ })?;
+ let (width, height) = img.dimensions();
+
+ for y in 0..height {
+     for x in 0..width {
+         let pixel = img.get_pixel(x, y);
+         let (r, g, b, _) = pixel.channels4();
+         write!(file, "{}s{}s{}p", r, g, b)?;
+     }
+     file.write_all(b"\n")?;
+ }
+
+ Ok(())
 }
+
