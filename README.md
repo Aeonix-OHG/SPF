fc535dccf14ec765931607c17eddf4766e68c331
Bonbon <mail@jan-schmitt.eu>
2023-12-29 18:16:25 +0100

    added command converting

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Neat Picture Format
 ## Concept
 The concept of NPF is to create a simple file in which are multiple multiple colors values for individiual pixels wich are seperated by a s character for the next value, a p character for next pixel or a n charcter for next line. For Example
 ```Concept
-255s051s047p000s000s000n
+255s051s047p000s000s000\n
 000s000s000p036s137s255
 ```
 ## Programm
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
 use image::{GenericImageView, Pixel};
 use std::fs::File;
 use std::io::Write;
+use std::env;
 
 fn main() -> std::io::Result<()> {
- let mut file = File::create("testpic.npf")?;
- let img = image::open("testpic.png").map_err(|e| {
-     std::io::Error::new(std::io::ErrorKind::Other, e.to_string())
- })?;
- let (width, height) = img.dimensions();
+    let args: Vec<String> = env::args().collect();
+    let path = &args[1];
+    let out_path = &args[2];
+    let mut file = File::create(out_path)?;
+    let img = image::open(path).map_err(|e| {
+        std::io::Error::new(std::io::ErrorKind::Other, e.to_string())
+    })?;
+    let (width, height) = img.dimensions();
+    for y in 0..height {
+        for x in 0..width {
+            let pixel = img.get_pixel(x, y);
+            let (r, g, b, _) = pixel.channels4();
+            write!(file, "{}s{}s{}p", r, g, b)?;
+        }
+        file.write_all(b"\n")?;
+    }
 
- for y in 0..height {
-     for x in 0..width {
-         let pixel = img.get_pixel(x, y);
-         let (r, g, b, _) = pixel.channels4();
-         write!(file, "{}s{}s{}p", r, g, b)?;
-     }
-     file.write_all(b"\n")?;
- }
-
- Ok(())
+    Ok(())
 }
 
